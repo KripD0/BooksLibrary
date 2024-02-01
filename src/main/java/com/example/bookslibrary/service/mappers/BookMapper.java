aa1301d4af55b134ed33f6eb791423af3f78a6fa
KripDo <danik2003235@mail.ru>
2024-02-01 22:35:12 +0300

    Fixed mapstruct bug

--- src/main/java/com/example/bookslibrary/service/mappers/BookMapper.java
+++ src/main/java/com/example/bookslibrary/service/mappers/BookMapper.java
@@ -3,13 +3,11 @@ package com.example.bookslibrary.service.mappers;
 import com.example.bookslibrary.db.model.Book;
 import com.example.bookslibrary.web.dto.BookDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
 
-    @Mapping(source = "title", target = "title")
     Book toBook(BookDto bookDto);
 
     void updateBook(@MappingTarget Book book, BookDto bookDto);
--- src/main/java/com/example/bookslibrary/service/mappers/PersonMapper.java
+++ src/main/java/com/example/bookslibrary/service/mappers/PersonMapper.java
@@ -3,13 +3,11 @@ package com.example.bookslibrary.service.mappers;
 import com.example.bookslibrary.db.model.Person;
 import com.example.bookslibrary.web.dto.PersonDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
 
-    @Mapping(target = "lastName", source = "lastName")
     Person toPerson(PersonDto personDto);
 
     void updatePerson(@MappingTarget Person person, PersonDto personDto);
